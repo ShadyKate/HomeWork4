d2fcb218dbbb8d223932f4b0dac97238b5cc2caa
Kate <katehappy16@gmail.com>
2023-12-04 14:11:48 +0300

    задача3

---
+++ task3/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+//Напишите программу, которая перевернёт одномерный массив 
+void FillArray(int [] arrayMadeUp)
+{
+    for(int i = 0; i < arrayMadeUp.Length; i++)
+    {
+        arrayMadeUp[i] = new Random().Next(1, 101);
+    }
+}
+void reverseArray(int [] arrayWeHave)
+{
+    for(int i = 0; i < arrayWeHave.Length / 2; i++)
+    {
+        int temp = arrayWeHave[i];
+        arrayWeHave[i] = arrayWeHave[arrayWeHave.Length - i -1];
+        arrayWeHave[arrayWeHave.Length - i - 1] = temp;
+    }
+}
+
+Console.WriteLine("Введите колличество элементов массива: ");
+int count = int.Parse(Console.ReadLine()!);
+int [] array = new int [count];
+FillArray(array);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+reverseArray(array);
+Console.WriteLine($"Перевёрнутый массив: [{string.Join(", ", array)}]");
